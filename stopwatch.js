4e5f8cf79ac596cf524455a58d32bcf619824b01
EC2 Default User <ec2-user@ip-172-31-74-0.ec2.internal>
2022-03-22 13:22:27 +0000

    リスタート機能の追加 second-commit

--- stopwatch.js
+++ stopwatch.js
@@ -1,72 +1,59 @@
-(function(){
+ 
+let display = document.getElementById('display');
+let start = document.getElementById('start');
+let stop = document.getElementById('stop');
+let reset = document.getElementById('reset');
   
-  let timer = document.getElementById('timer');
-  let start = document.getElementById('start');
-  let stop = document.getElementById('stop');
-  let reset = document.getElementById('reset');
-  
-  let startTime;
-  
-  let elapsedTime = 0;
-  
-  let timerId;
-  
-  let timeToadd = 0 ;
-  
-function updateTimeText(){
+let startTime;
+let millisecond = 0;
+let second = 0;
+let tensecond = 0;
+let hundred = 0;
+//リスタート機能の追加
+function timer(){
+    millisecond++;
 
-  let minutes = Math.floor(elapsedTime / 60000);
-  
-  let second = Math.floor(elapsedTime % 60000 / 1000);
-  
-  let milliSecond = Math.floor(elapsedTime % 1000);
-  
-  minutes = ('0'+ minutes).slice(-2);
-  second = ('0' + second).slice(-2);
-  milliSecond = ('0' + milliSecond).slice(-2)
+	if (millisecond / 10 == 1){
+		second++;
+		millisecond = 0;
+		 　　if(second / 10 == 1){
+            　　tensecond++;
+				second = 0;
+				　　if(tensecond / 10 == 1){
+					 　hundred++;
+					 　tensecond 	= 0;
+				 　　}
+		 　　}
+	 }
+ display.innerHTML= hundred + ":" + tensecond + ":" + second + ":" + millisecond;
+};
 
-  timer.textContent = minutes + ':' + second + ':' + milliSecond;
-  
-}
-  
-function countUp(){
 
-  timerId = setTimeout(function(){
-    
-    elapsedTime = Date.now() - startTime + timeToadd;
-    updateTimeText()
-    
-    countUp();
-  },10);  
-}
-
-document.getElementById("start").disabled = false;
-document.getElementById("stop").disabled = true;
-document.getElementById("reset").disabled = true;
-
-document.getElementById('start').onclick = function(){
-  startTime = Date.now();
-  countUp();
-  document.getElementById("start").disabled = true;
+//スタートボタン
+document.getElementById('start').onclick = function startButton(button){
+  startTime = setInterval(timer,100);
+	document.getElementById("start").disabled = true;
   document.getElementById("stop").disabled = false;
   document.getElementById("reset").disabled = true;
 };
 
-document.getElementById('stop').onclick = function(){
-  clearTimeout(timerId);
-  timeToadd += Date.now() + startTime;
-  document.getElementById("start").disabled = true;
+//ストップボタン
+document.getElementById('stop').onclick = function stopButton(button){
+  clearInterval(startTime);
+	document.getElementById('stop').onclick
+  document.getElementById("start").disabled = false;
   document.getElementById("stop").disabled = true;
   document.getElementById("reset").disabled = false;
 };
 
-document.getElementById('reset').onclick = function(){
-  elapsedTime = 0;
-  timeToadd = 0;
-  updateTimeText();
-  document.getElementById("start").disabled = false;
+//リセットボタン
+document.getElementById('reset').onclick = function buttonC(buttton){
+	millisecond = 0;
+    second = 0;
+    tensecond = 0;
+    hundred = 0;
+	display.innerHTML= hundred + ":" + tensecond + ":" + second + ":" + millisecond;
+	document.getElementById("start").disabled = false;
   document.getElementById("stop").disabled = true;
   document.getElementById("reset").disabled = true;
-};
-
-})();
+}
